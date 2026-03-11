a4cc864962fd4abaa0b7d17c06e56fb61694ba7a
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2026-03-11 15:06:57 +0100

    Commited

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,13 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("")
+	mul := &MultiplicationTable{}
+	mul.SetTable(0, 0, nil)
+	//mulTable := mul.CreateTable1()
+	//fmt.Println(mulTable)
+	mul.createTable2()
+
+}
--- cmd/app/multiplicationTable.go
+++ cmd/app/multiplicationTable.go
@@ -1,19 +1,42 @@
 package main
 
-type multiTable interface {
+import "fmt"
+
+type MultiTable interface {
+	GetTable() (int, int, [][]int)
+	CreateTable() [][]int
 }
 
-type multiplicationTable struct {
+type MultiplicationTable struct {
 	multiplierNum, multiplicandNum int
+	mTable                         [][]int
 }
 
-func (m *multiplicationTable) SetMultiplicationTable(multiplierNum, multiplicandNum int) {
+func (m *MultiplicationTable) SetTable(multiplierNum, multiplicandNum int, mTable [][]int) {
 	m.multiplierNum = multiplierNum
 	m.multiplicandNum = multiplicandNum
+	m.mTable = make([][]int, 9)
 }
 
-func (m *multiplicationTable) GetMultiplicationTable() (int, int) {
-	return m.multiplierNum, m.multiplicandNum
+func (m *MultiplicationTable) GetTable() (int, int, [][]int) {
+	return m.multiplierNum, m.multiplicandNum, m.mTable
 }
 
-func (m *multiplicationTable) CreateMultiTable() {}
+func (m *MultiplicationTable) CreateTable1() [][]int {
+	m.mTable = make([][]int, 10)
+	for m.multiplierNum = 0; m.multiplierNum <= 9; m.multiplierNum++ {
+		m.mTable[m.multiplierNum] = make([]int, 10)
+		for m.multiplicandNum = 0; m.multiplicandNum <= 9; m.multiplicandNum++ {
+			m.mTable[m.multiplierNum][m.multiplicandNum] = m.multiplierNum * m.multiplicandNum
+		}
+	}
+	return m.mTable
+}
+
+func (m *MultiplicationTable) createTable2() {
+	for i := 0; i <= 9; i++ {
+		for j := 0; j <= 9; j++ {
+			fmt.Printf("%2d|", i*j)
+		}
+	}
+}
