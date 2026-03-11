5233f402fb22ad69515861c7d12905bd545819c0
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2026-03-11 18:04:19 +0100

    Commited

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,23 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("")
-	/*
-			mul := &MultiplicationTable{}
-			mul.SetTable(0, 0, nil)
-			//mulTable := mul.CreateTable1()
-			//fmt.Println(mulTable)
-			mul.createTable2()
+	choice := 0
 
-		cp := &CheckPointer{}
-		cp.SetCheckPointer(0, 0, nil)
-		cp.exeCheckPointer()
-	*/
+	fmt.Println("please choice to execute programm.")
+	for {
+		fmt.Println("\n1: MultiplicationTable, 2: CheckPointer, 3:BMI-calculator, 0: end. ")
+		if _, err := fmt.Scan(&choice); err == nil {
+			if choice == 0 {
+				println("bye bye.")
+				break
+			}
 
-	bmi := &Bmi{}
-	bmi.SetBmi(0.0, 0.0, 0.0, "")
-	bmi.CalcBmi()
+			switch choice {
+			case 1:
+				fmt.Println("execute MultiplicationTable")
+				mul := &MultiplicationTable{}
+				mul.SetTable(0, 0, nil)
+				//mulTable := mul.CreateTable1()
+				//fmt.Println(mulTable)
+				mul.createTable2()
+				continue
+			case 2:
+				fmt.Println("execute CheckPointer")
+				cp := &CheckPointer{}
+				cp.SetCheckPointer(0, 0, nil)
+				cp.exeCheckPointer()
+				continue
+			case 3:
+				fmt.Println("execute BMI-calculator")
+				bmi := &Bmi{}
+				bmi.SetBmi(0.0, 0.0, 0.0, "")
+				bmi.CalcBmi()
+				continue
+			}
+
+		}
+		fmt.Println("Invalid input. please input a number again.")
+		var dump string
+		fmt.Scanln(&dump) // clear buffer
+	}
 
 }
